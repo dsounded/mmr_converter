eec54ea80cab32911bf1d766abcca435818b4c3b
Kiril Dokh <dsounded@gmail.com>
2018-07-18 18:11:43 +0700

    Add removing/editing dogs functionality

---
+++ src/components/dog_edit_box.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { saveDog, changeDog } from '../actions'
+
+const DogList = ({dispatch, value, initialValue}) => {
+  return (
+    <span>
+      <input type="text" value={value} onChange={e => dispatch(changeDog(initialValue, e.target.value))} />
+      <a href='#' onClick={e =>{dispatch(saveDog(initialValue, value))}}>Save</a>
+    </span>
+  )
+}
+
+export default DogList
